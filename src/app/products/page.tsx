c158fe6c041827215e5b8d2860aebe48c5e5ddf1
Caio Santos <caiosantosxp@gmail.com>
2025-02-19 19:51:35 -0300

    feat: add shadcninitial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const ProductPage = () => {
+  return (
+    <div>
+      <Button>Salvar</Button>
+    </div>
+  )
+}
+
+export default ProductPage;
